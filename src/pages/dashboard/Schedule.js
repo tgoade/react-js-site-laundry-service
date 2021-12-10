d2289c4c3c54d1d55153db808231451813679231
tmdigitalspace <88467417+tmdigitalspace@users.noreply.github.com>
2021-12-10 09:54:23 -0500

    Testing new api url

--- src/pages/dashboard/Schedule.js
+++ src/pages/dashboard/Schedule.js
@@ -12,7 +12,7 @@ const Schedule = () => {
         e.preventDefault();
         console.log(`uid with submitHandler: ${uid}`); 
         setLoading(true);
-        Axios.post("https://app-bootcamp-davidgoade.twiliobootcamp.repl.co/pickup", { uid: uid })
+        Axios.post("http://ad700220bd2214bbbb43cf63ad15e933-711171792.us-west-1.elb.amazonaws.com/pickup", { uid: uid })
             .then(res => { console.log(res.data) })
             .catch(error => { console.error("There's an error:", error)})
         }
